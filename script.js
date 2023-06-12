09589b801b5235539fc7573d178a2cffed715670
rafaelbarcarol <rafaelbarcarol@hotmail.com>
2023-06-12 15:43:53 -0300

    added e-mail validation feature

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const email = document.querySelector("#email");
 const actionBtn = document.querySelector(".actionBtn");
 
-actionBtn.addEventListener("click", () => {
-  alert("Esse é um projeto fictício de estudo.");
+function validateEmail(email) {
+  if (!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)) {
+    const error = new Error();
+    error.input = "email";
+    throw error;
+  }
+}
+
+actionBtn.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  email.classList.remove("success", "error");
+
+  try {
+    validateEmail(email.value);
+    email.classList.add("success");
+    alert("Esse é um projeto fictício de estudo.");
+  } catch (error) {
+    email.classList.add("error");
+    alert("Formato de e-mail inválido.");
+  }
 });
