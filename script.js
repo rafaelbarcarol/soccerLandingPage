865226a7ec91c11348f6f606180a021cde4700bd
rafaelbarcarol <rafaelbarcarol@hotmail.com>
2023-06-13 18:46:29 -0300

    update form details

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const email = document.querySelector("#email");
+const username = document.querySelector("#name");
+const birthDate = document.querySelector("#birthDate");
 const actionBtn = document.querySelector(".actionBtn");
 
 function validateEmail(email) {
@@ -13,12 +15,16 @@ actionBtn.addEventListener("click", (ev) => {
   ev.preventDefault();
   email.classList.remove("success", "error");
 
-  try {
-    validateEmail(email.value);
-    email.classList.add("success");
-    alert("Esse é um projeto fictício de estudo.");
-  } catch (error) {
-    email.classList.add("error");
-    alert("Formato de e-mail inválido.");
+  if (email.value === "" && username.value === "" && birthDate.value === "") {
+    alert("Preencha o formulário e tente novamente.");
+  } else {
+    try {
+      validateEmail(email.value);
+      email.classList.add("success");
+      alert("Esse é um projeto fictício de estudo.");
+    } catch (error) {
+      email.classList.add("error");
+      alert("Formato de e-mail inválido.");
+    }
   }
 });
